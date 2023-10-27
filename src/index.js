07fe2ecd21b74a9c830f421f90923b9cfd32659d
Vladimir Minakov <33041336+VladimirMV@users.noreply.github.com>
2023-10-27 14:23:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RegistryFront">
+    <BrowserRouter basename="RegistryFront">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={theme}>
